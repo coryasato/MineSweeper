5e78f0fb28d772089953d3854890c437b8ba565b
Cory Asato <czasato@gmail.com>
2015-03-10 22:31:27 -0700

    Adds security to Express

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 var express     = require('express'),
     path        = require('path'),
     bodyParser  = require('body-parser'),
-    cors        = require('express-cors');
+    cors        = require('express-cors'),
+    compress    = require('compression'),
+    helmet      = require('helmet');
 
 var app = express();
 // JSX transpiler
@@ -14,6 +16,14 @@ require('node-jsx').install();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// Compress { Place before express.static }
+app.use(compress({
+    filter: function(req, res) {
+        return (/json|text|javascript|css/).test(res.getHeader('Content-Type'));
+    },
+    level: 9
+}));
+
 app.use(express.static(path.join(__dirname, 'dist')));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -21,6 +31,15 @@ app.set('view engine', 'ejs');
 app.use(cors({
   allowedOrigins: ['http://fonts.googleapis.com']
 }));
+
+// Helmet to secure Express headers
+app.use(helmet.xframe());
+app.use(helmet.xssFilter());
+app.use(helmet.nosniff());
+app.use(helmet.ienoopen());
+app.disable('x-powered-by');
+
+app.enable('trust proxy');
 
 /**
  * Routes
